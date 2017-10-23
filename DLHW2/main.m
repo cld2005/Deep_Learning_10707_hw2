d3fa80ce523d1dcca2e00a3624b83b7c4e68deb9
Lindi Chen <lindic@bh140d-b1.andrew.cmu.edu>
2017-10-23 16:06:49 -0400

    push

--- DLHW2/main.m
+++ DLHW2/main.m
@@ -7,10 +7,10 @@ for i=1:rounds
     num_visible=784;
     num_hiddenn=100;
     learning_rate=0.1;
-    batch_size=1;
-    epoches=200;
-    k=1;
-    rbms{i} = RBM();
+    batch_size=200;
+    epoches=1000 ;
+    k=5;
+    rbms{i} = RBM_gpu();
     [train_error,vali_error] = rbms{i}.train(num_visible,num_hiddenn,learning_rate,batch_size,epoches,k);
     filter_plot(rbms{1},100,i);
     rbms{i}.clear_data();
--- DLHW2/main_5g_rbm.m
+++ DLHW2/main_5g_rbm.m
@@ -6,19 +6,15 @@ rbms=cell(rounds,1);
 
 for i=1:rounds
     
-    if i==2
-        continue
-    end
-    
     fprintf('round %d\n',i );
 
     num_visible=784;
     num_hiddenn=num_hiddenn_sizes(i);
     learning_rate=0.1;
-    batch_size=1;
+    batch_size=100;
     epoches=200;
     k=1;
-    rbms{i} = RBM();
+    rbms{i} = RBM_gpu();
     [train_error,vali_error] = rbms{i}.train(num_visible,num_hiddenn,learning_rate,batch_size,epoches,k);
     %filter_plot(rbms{i},num_hiddenn,i);
     rbms{i}.clear_data();
